74a9712364cdd6b87e8a244bf9f149485e29224f
Faizal-work <faizal.work@proton.me>
2026-02-12 15:36:49 +0800

    Changed from lambda to route mthods. Cleaner

--- Readme.md
+++ Readme.md
@@ -2,4 +2,7 @@
 Evaluating minimal APIs vs MVC controllers for microservices and updating my own understanding
 
 # 2026-January-12
-First commit into master
+1. First commit into master
+2. Created the minimal API codes
+3. Updated the code to MapGroup and calling from Method instead of Lambda
+4. Returning using TypedResults
--- min_api/Program.cs
+++ min_api/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 // Grouping the app and using TypedResult/
 var todoItems = app.MapGroup("/todoitems");
 
+// Methods for the todo list
 todoItems.MapGet("/", GetAllTodos);
 todoItems.MapGet("/complete", GetCompleteTodos);
 todoItems.MapGet("/{id}", GetTodo);
@@ -36,4 +37,50 @@ static async Task<IResult> GetTodo(int id, TodoDb db)
 {
     return await db.Todos.FindAsync(id)
         is Todo todo ? TypedResults.Ok() : TypedResults.NotFound();
+}
+
+// Adding into todo
+static async Task<IResult> CreateTodo( Todo todo, TodoDb db)
+{
+    if (string.IsNullOrWhiteSpace(todo.Name))
+    {
+        return Results.NoContent();
+    }
+
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
+
+// Update
+static async Task<IResult> UpdateTodo(int id, Todo todoInput, TodoDb db)
+{
+    if (string.IsNullOrWhiteSpace(todoInput.Name))
+    {
+        return Results.NoContent();
+    }
+
+    var todoNew = await db.Todos.FindAsync(id);
+    if (todoNew != null) {
+        return TypedResults.NotFound();
+    }
+
+    todoNew?.Name = todoInput.Name;
+    todoNew?.IsComplete = todoInput.IsComplete;
+
+    await db.SaveChangesAsync();
+    return TypedResults.Ok();
+}
+
+// Delete task
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
 }
