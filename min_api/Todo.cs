7613f8ec66494496dfa7e20d5a145cecf37c92d9
Faizal-work <faizal.work@proton.me>
2026-02-12 11:00:00 +0800

    First Push

---
+++ min_api/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace min_api
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ min_api/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace min_api
+{
+    // DbContext is the database manager between C# to any database
+    class TodoDb: DbContext
+    {
+        // Initialisation for DBContext options
+        public TodoDb(DbContextOptions<TodoDb> options): base(options) {}
+
+        // Setting the DB queries i.e. Todos.Where, Todos.FindAllSync etc
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
